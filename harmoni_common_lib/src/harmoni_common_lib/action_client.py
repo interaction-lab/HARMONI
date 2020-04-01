3b70eb3ce5e0499205437f5771e59c61a26845a9
chrismbirmingham <cmbirmingham19@gmail.com>
2020-04-01 15:38:19 -0700

    clean up naming on action client

--- harmoni_common_lib/src/harmoni_common_lib/action_client.py
+++ harmoni_common_lib/src/harmoni_common_lib/action_client.py
@@ -12,14 +12,19 @@ class HarmoniActionClient():
     Controllers and manager are clients.
     This class provides basic client functionality which controller and manager extend,
     including basic type checking, warnings, interrupts, etc.
-
     """
     def __init__(self):
         """ Initialization of the variables """
-        self.init_check_variables_client()
+        self.set_com_flag_variables()
+
+    def set_com_flag_variables(self):
+        """ Initizalize or reset of communication flag variables """
+        self.result_received = False
+        self.feedback_received = False
+        return
 
     def init_action_variables(self):
-        """ Initizalization or Reset variables """
+        """ Initizalize or reset action msg variables """
         self.action_result = {}
         self.action_feedback = {}
         self.action_result = {
@@ -30,17 +35,17 @@ class HarmoniActionClient():
             "state": ""
         }
 
-    def setup_client(self, action_topic, timeout, execute_goal_result_callback, execute_goal_feedback_callback):
-        """ Init client action variables and setup clients"""
-        timeout = rospy.Duration.from_sec(timeout)
+    def setup_client(self, action_type_name, execute_goal_result_callback, execute_goal_feedback_callback, wait=True):
+        """ Init client action variables and setup client """
         self.init_action_variables()
-        self.action_client = actionlib.SimpleActionClient(action_topic, harmoniAction)
-        self.action_client.wait_for_server(timeout)
+        self.action_client = actionlib.SimpleActionClient(action_type_name, harmoniAction)
+        if wait:
+            self.action_client.wait_for_server()
         self.execute_goal_result_callback = execute_goal_result_callback
         self.execute_goal_feedback_callback = execute_goal_feedback_callback
         return
 
-    def goal_result_callback(self, terminal_state, result):
+    def action_result_callback(self, terminal_stale, result):
         """ Save the action result """
         rospy.loginfo("Heard back result from: " + result.action)
         self.action_result["do_continue"] = result.do_continue
@@ -48,12 +53,28 @@ class HarmoniActionClient():
         self.execute_goal_result_callback(self.action_result)
         return
 
-    def goal_feedback_callback(self, feedback):
+    def action_feedback_callback(self, feedback):
         """ Save the action feedback """
         rospy.loginfo("Heard back feedback from: " + feedback.action)
         self.action_feedback["state"] = feedback.state
         self.execute_goal_feedback_callback(self.action_feedback)
         return
+
+    def feedback_received(self):
+        """ Check if feedback received """
+        if self.feedback_received:
+            self.feedback_received = False
+            return True
+        else:
+            return False
+
+    def result_received(self):
+        """ Check if result received """
+        if self.result_received:
+            result = True
+        else:
+            result = False
+        return result
 
     def feedback_data(self):
         """Return Feedback Data"""
@@ -65,8 +86,8 @@ class HarmoniActionClient():
 
     def send_goal(self, action_goal, optional_data, child, condition, timeout):
         """ Reset of check variables. Send goal and set the time out """
-        self.init_check_variables_client()
+        self.set_com_flag_variables()
         goal = harmoniGoal(action=action_goal, optional_data=optional_data, child=child, condition=condition)
-        self.action_client.send_goal(goal, done_cb=self.goal_result_callback, feedback_cb=self.goal_feedback_callback)
+        self.action_client.send_goal(goal, done_cb=self.action_result_callback, feedback_cb=self.action_feedback_callback)
         self.action_client.wait_for_result(rospy.Duration.from_sec(timeout))
         return
