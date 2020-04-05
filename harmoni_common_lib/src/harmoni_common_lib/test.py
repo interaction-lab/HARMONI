c98ff36e432964e8fa2c5d3a3719551afd598c3f
Chris <cmbirmingham19@gmail.com>
2020-04-04 17:14:02 -0700

    fix spacing

--- harmoni_common_lib/src/harmoni_common_lib/test.py
+++ harmoni_common_lib/src/harmoni_common_lib/test.py
@@ -7,14 +7,16 @@ import actionlib
 from action_client import HarmoniActionClient
 from action_server import HarmoniActionServer
 
+
 class Controller(HarmoniActionServer):
     """
     Testing the callback function
     """
+
     def __init__(self):
         print("Init Controller")
         return
-    
+
     def callback_controller(self, request_data):
         print("Handle controller")
         return
@@ -23,19 +25,20 @@ class Controller(HarmoniActionServer):
         print("Setup")
         self.setup_server("controller", self.callback_controller)
         return
-    
+
     def callback_result(self):
         print("Result")
 
     def callback_feedback(self):
         print("Feedback")
 
+
 if __name__ == "__main__":
     rospy.init_node("test_node")
     contr = Controller()
     contr.setup()
     client = HarmoniActionClient()
-    client.setup_client("controller", timeout= 5, contr.callback_result(), contr.callback_feedback())
-    client.send_goal(action_goal="test", optional_data="ciao", child="", condition= "", time_out=20)
+    client.setup_client("controller", timeout=5, contr.callback_result(), contr.callback_feedback())
+    client.send_goal(action_goal="test", optional_data="ciao", child="", condition="", time_out=20)
     rospy.spin()
     pass
