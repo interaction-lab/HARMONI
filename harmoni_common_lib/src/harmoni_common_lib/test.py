373fc5fb212c76a0c2ddf790683057d26805f4c0
Chris <cmbirmingham19@gmail.com>
2020-04-07 21:41:43 -0700

    Debugging/printing (test is not working yet)

--- harmoni_common_lib/src/harmoni_common_lib/test.py
+++ harmoni_common_lib/src/harmoni_common_lib/test.py
@@ -8,7 +8,6 @@ from harmoni_common_msgs.msg import harmoniAction, harmoniFeedback, harmoniResul
 from action_client import HarmoniActionClient
 
 
-
 class HarmoniActionServer(object):
     """
     Most nodes (both controllers and children) are servers.
@@ -18,20 +17,23 @@ class HarmoniActionServer(object):
     """
 
     def __init__(self):
+        print("Init HAS")
         """Initialization"""
 
     def setup_server(self, action_topic):
         """You must know the action name to set up a server"""
+        print("setup_server HAS")
         self.__feedback = harmoniFeedback()
         self.__result = harmoniResult()
         self.action_topic = action_topic
         self.__action_server = actionlib.SimpleActionServer(self.action_topic, harmoniAction, self.goal_received_callback, auto_start=False)
         self.__action_server.start()
-        rospy.loginfo("Server starts")
+        rospy.loginfo("Server started with name: " + self.action_topic)
         return
 
     def goal_received_callback(self, goal):
         """ Callback function, initialize the variables and set the goal to received"""
+        print("goal_received_callback HAS")
         self.action_goal = goal.action  # action request
         self.optional_data = goal.optional_data  # input data for the module
         self.child = goal.child  # external module that will accomplish the task
@@ -41,6 +43,7 @@ class HarmoniActionServer(object):
 
     def send_feedback(self, state):
         """ Send the feedback"""
+        print("send_feedback HAS")
         self.__feedback.action = self.action_goal
         self.__feedback.state = state
         self.__action_server.publish_feedback(self.__feedback)
@@ -49,12 +52,14 @@ class HarmoniActionServer(object):
 
     def send_result(self, do_continue, message):
         """Send the result and action set to succeded"""
+        print("send_result HAS")
         self.__result.action = self.action_goal
         self.__result.do_continue = do_continue
         self.__result.message = message
         self.__action_server.set_succeeded(self.__result)
         rospy.loginfo("The action " + self.__result.action + " have been set to succeded")
         return
+
 
 class Controller(HarmoniActionServer):
     """
@@ -65,29 +70,35 @@ class Controller(HarmoniActionServer):
         print("Init Controller")
         return
 
-    def callback_controller(self, request_data):
-        print("Handle controller")
-        super(Controller, self).goal_received_callback()
+    # def callback_controller(self, request_data):
+    #     print("Handle controller")
+    #     super(Controller, self).goal_received_callback()
+    #     return
+
+    def setup(self, name):
+        print("Setup Controller")
+        self.setup_server(name)
         return
 
-    def setup(self):
-        print("Setup")
-        self.setup_server("controller")
+    def callback_result(self, _):
+        print("callback_result Controller")
         return
 
-    def callback_result(self):
-        print("Result")
-
-    def callback_feedback(self):
-        print("Feedback")
+    def callback_feedback(self, _):
+        print("callback_feedback Controller")
+        return
 
 
 if __name__ == "__main__":
     rospy.init_node("test_node")
     contr = Controller()
-    contr.setup()
+    print("SETUP CONTROLLER ************")
+    contr.setup("test")
+    print("SETUP CLIENT ************")
     client = HarmoniActionClient()
-    client.setup_client("controller", contr.callback_result(), contr.callback_feedback())
+    client.setup_client("test", contr.callback_result, contr.callback_feedback, False)
+    print("CLIENT SETUP COMPLETE - SEND GOAL ************")
     client.send_goal(action_goal="test", optional_data="ciao", child="", condition="", time_out=20)
+    print("DONE ************")
     rospy.spin()
     pass
