1c06704ee925eabfbf53642c9fe5c83e9b5f232e
micolspitale93 <micol.spitale@gmail.com>
2020-04-15 16:40:30 +0200

    removed cordial face

--- cordial_face/scripts/setup_http_servers.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-killall http-server
-
-source /opt/ros/kinetic/setup.bash
-source /home/qtrobot/catkin_ws/devel/setup.bash
-
-roscd cordial_face/web && http-server -p $1 &
-roscd cordial_tablet/web && http-server -p $2 &
