2baad3017bb8873211c0cb82586056ec6bfe3ccb
micolspitale93 <micol.spitale@gmail.com>
2020-04-22 11:55:28 +0200

    hardware reading send result failed if is stopped.

--- harmoni_common_lib/src/harmoni_common_lib/child.py
+++ harmoni_common_lib/src/harmoni_common_lib/child.py
@@ -160,6 +160,8 @@ class InternalServiceServer(HarmoniActionServer, object):
                     self.send_feedback(self.service_manager.state)
                 rospy.Rate(.2)
             else:
+                self.send_result(do_action=False,
+                    message=self.service_manager.state)
                 break
         return
 
@@ -209,6 +211,8 @@ class HarwareReadingServer(HarmoniActionServer, object):
                     self.send_feedback(self.service_manager.state)
                 rospy.Rate(.2)
             else:
+                 self.send_result(do_action=False,
+                    message=self.service_manager.state)
                 break
         return
 
