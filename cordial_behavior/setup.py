bdf7c30061d4efd2dfcc48535d2a5412940baf0b
Chris <cmbirmingham19@gmail.com>
2020-04-27 21:13:51 -0700

    reorganize for catkin build purposes

--- cordial_behavior/setup.py
+++ cordial_behavior/setup.py
@@ -1,5 +1,5 @@
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
-## See http://ros.org/doc/api/catkin/html/user_guide/setup_dot_py.html
+# ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+# See http://ros.org/doc/api/catkin/html/user_guide/setup_dot_py.html
 
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
@@ -7,7 +7,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['cordial_behavior'],
-    package_dir={'': 'src'}
+    package_dir={'': 'scripts'}
 )
 
 setup(**setup_args)
