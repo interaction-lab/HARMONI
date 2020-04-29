22573a6519f80bd8dca1d416b88e639c1004ede6
micolspitale93 <micol.spitale@gmail.com>
2020-04-29 11:19:38 +0200

    harmoni web package works

---
+++ harmoni_web/scripts/harmoni_web/http_server_runner.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+"""
+This script takes two arguements: a path to where it should open the http-server and the desired port.
+If no port is specified, http-server will pick.
+If the port selected is in use, you will see a long javascript error.
+When the script is terminated, it kills the http-server that it has created
+
+Usage example:
+
+    python http_server_runner.py /path/to/dir 8080
+
+"""
+
+import rospy
+import subprocess
+import atexit
+import os
+import sys
+
+
+def is_int(value):
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+
+
+DIR_IDX = 1
+PORT_IDX = 2
+if __name__ == '__main__':
+
+    args = sys.argv
+    if os.path.exists(args[DIR_IDX]):
+        directory = args[DIR_IDX]
+        os.chdir(directory)
+    else:
+        raise ValueError("Invalid directory input")
+
+    command = ["http-server"]
+    if is_int(args[PORT_IDX]):
+        port = args[PORT_IDX]
+        command += ["-p", port]
+
+    p = subprocess.Popen(command)
+    atexit.register(p.terminate)
+
+    while not rospy.is_shutdown():
+        rospy.sleep(1)
