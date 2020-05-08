60d773f0cc963e2867d76bdb612de7ed502fa217
micolspitale93 <micol.spitale@gmail.com>
2020-05-08 12:30:41 +0200

    add routers to constants

--- harmoni_common_lib/src/harmoni_common_lib/constants.py
+++ harmoni_common_lib/src/harmoni_common_lib/constants.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Importing the libraries
-from enum import IntEnum
+from enum import IntEnum, Enum
 
 class ActionType(IntEnum):
     ON = 1
@@ -15,3 +15,26 @@ class State(IntEnum):
     REQUEST = 2
     SUCCESS = 3
     FAILED = 4
+
+class Router(Enum):
+    SENSOR = "sensor"
+    DIALOGUE = "dialogue"
+    ACTUATOR = "actuator"
+    #DETECTOR = "detector"
+
+class RouterSensor(IntEnum):
+    pc_microphone = 1
+    pc_camera = 2
+
+class RouterActuator(IntEnum):
+    pc_speaker = 1
+    pc_face = 2
+    tts = 3
+    web = 4
+
+class RouterDialogue(IntEnum):
+    lex = 1
+
+
+class RouterDetector(IntEnum):
+    stt = 1
