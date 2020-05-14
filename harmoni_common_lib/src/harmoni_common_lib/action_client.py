ed21d168565d0fb3554c4b4d28d35a041a510c6a
chrismbirmingham <cmbirmingham19@gmail.com>
2020-05-14 14:18:53 -0700

    clean up comments and organization for action wrap

--- harmoni_common_lib/src/harmoni_common_lib/action_client.py
+++ harmoni_common_lib/src/harmoni_common_lib/action_client.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-# Importing the libraries
 import rospy
 import roslib
 from actionlib import SimpleActionClient
@@ -10,49 +9,31 @@ from harmoni_common_msgs.msg import harmoniGoal, harmoniAction
 class HarmoniActionClient(object):
     """A wrapper around SimpleActionClient that is structured for HARMONI architecture.
 
-    Controllers and manager are clients.
-    This class provides basic client functionality which controller and manager extend,
+    Routers and managers are clients.
+    This class provides basic client functionality which routers and managers extend,
     including basic type checking, warnings, interrupts, etc.
     """
+
     def __init__(self):
         """ Initialization of the variables """
         self._init_action_variables()
         self._set_com_flag_variables()
 
     def _set_com_flag_variables(self):
+        """ Set communication flag variables to default: False """
         self.feedback_received = False
         self.result_received = False
         return
 
     def _init_action_variables(self):
         """ Initizalize or reset action msg variables """
-        self.action_result = {}
-        self.action_feedback = {}
-        self.action_result = {
+        self._action_result = {
             "do_action": False,
             "message": ""
         }
-        self.action_feedback = {
+        self._action_feedback = {
             "state": ""
         }
-
-    def _result_callback(self, terminal_state, result):
-        """Save the action result """
-        rospy.loginfo("Heard back result from: %i" %result.action_type)
-        self.action_result["do_action"] = result.do_action
-        self.action_result["message"] = result.message
-        if (self.execute_goal_result_callback):
-            self.execute_goal_result_callback(self.action_result)
-        self.result_received = True
-        return
-
-    def _feedback_callback(self, feedback):
-        """Save the action feedback """
-        rospy.logdebug("Heard back feedback from: %i"  %feedback.action_type)
-        self.action_feedback["state"] = feedback.state
-        if (self.execute_goal_feedback_callback):
-            self.execute_goal_feedback_callback(self.action_feedback)
-        self.feedback_received = True
         return
 
     def setup_client(self, action_type_name, execute_goal_result_callback=None, execute_goal_feedback_callback=None, wait=True):
@@ -66,11 +47,14 @@ class HarmoniActionClient(object):
                 handle for action feedback.
             wait (bool): Indicates whether
         """
+
         self._init_action_variables()
+
         self.action_client = SimpleActionClient(action_type_name, harmoniAction)
         if wait:
             rospy.loginfo("action_client waiting for {} server to connect.".format(action_type_name))
             self.action_client.wait_for_server()
+
         self.execute_goal_result_callback = execute_goal_result_callback
         self.execute_goal_feedback_callback = execute_goal_feedback_callback
         rospy.loginfo("action_client {} server connected.".format(action_type_name))
@@ -82,7 +66,8 @@ class HarmoniActionClient(object):
         Returns:
             dictionary: feedback message content 
         """
-        return(self.action_feedback)
+
+        return(self._action_feedback)
 
     def get_result_data(self):
         """Return Result Data
@@ -90,7 +75,8 @@ class HarmoniActionClient(object):
         Returns:
             dictionary: result message content 
         """
-        return(self.action_result)
+
+        return(self._action_result)
 
     def send_goal(self, action_goal, optional_data="", child_server="", condition="", time_out=600):
         """Sends a goal to the action server tied to this client.
@@ -100,8 +86,34 @@ class HarmoniActionClient(object):
             optional_data: 
         Reset of check variables. Send goal and set the time out 
         """
+
         self._set_com_flag_variables()
+
         goal = harmoniGoal(action_type=action_goal, optional_data=optional_data, child_server=child_server, condition=condition)
+        
         self.action_client.send_goal(goal, done_cb=self._result_callback, feedback_cb=self._feedback_callback)
         self.action_client.wait_for_result(rospy.Duration.from_sec(time_out))
+        return
+
+    def _result_callback(self, terminal_state, result):
+        """ Save the action result and perform the callback if set """
+        rospy.loginfo("Heard result from: %i" % result.action_type)
+        self._action_result["do_action"] = result.do_action
+        self._action_result["message"] = result.message
+
+        if (self.execute_goal_result_callback):
+            self.execute_goal_result_callback(self._action_result)
+
+        self.result_received = True
+        return
+
+    def _feedback_callback(self, feedback):
+        """ Save the action feedback and perform the callback if set """
+        rospy.logdebug("Heard back feedback from: %i" %feedback.action_type)
+        self._action_feedback["state"] = feedback.state
+
+        if (self.execute_goal_feedback_callback):
+            self.execute_goal_feedback_callback(self._action_feedback)
+
+        self.feedback_received = True
         return
--- harmoni_common_lib/src/harmoni_common_lib/action_server.py
+++ harmoni_common_lib/src/harmoni_common_lib/action_server.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-# Importing the libraries
 import rospy
 import roslib
 import actionlib
@@ -10,8 +9,8 @@ from harmoni_common_msgs.msg import harmoniAction, harmoniFeedback, harmoniResul
 class HarmoniActionServer(object):
     """A wrapper around SimpleActionServer that is structured for HARMONI architecture.
 
-    Most nodes (both controllers and children) are servers.
-    This class provides basic server functionality which controllers and children extend,
+    Most nodes (both routers and children) are servers.
+    This class provides basic server functionality which routers and children extend,
     including basic type checking, warnings, interrupts, etc.py
     
     TODO: Implement priority handling for received goals instead of preempting current goals.
@@ -19,45 +18,33 @@ class HarmoniActionServer(object):
     """
 
     def _init_(self):
-        """Initialization"""
+        """No initialization required, server setup must be called explicitly """
 
+    # You must know the action name to set up a server
     def setup_server(self, action_topic, execute_goal_received_callback):
-        """You must know the action name to set up a server"""
+        """ Set up action server with appropriate action topic"""
         self._feedback = harmoniFeedback()
         self._result = harmoniResult()
         self.action_topic = action_topic
+        # autostart can be buggy, so start is called explicitly 
         self._action_server = actionlib.SimpleActionServer(self.action_topic, harmoniAction, self._goal_received_callback, auto_start=False)
         self._action_server.start()
-        rospy.loginfo("Server starts")
         self.execute_goal_received_callback = execute_goal_received_callback
+        rospy.loginfo(f"{action_topic} server started")
         return
 
     def _goal_received_callback(self, goal):
-        """ Callback function, initialize the variables and set the goal to received"""
+        """ Save the goal data, set the goal to received, and execute the child callback """
         self.action_goal = goal.action_type  # action request
         self.optional_data = goal.optional_data  # input data for the module
         self.child = goal.child_server  # external module that will accomplish the task
         self.condition = goal.condition  # event condition to wait before starting the action
-        rospy.loginfo("The goal is: %i" %goal.action_type)
+        
         self.goal_received = True
+        rospy.loginfo("The goal is: %i" % goal.action_type)
+        # Perform the callback set by child
         self.execute_goal_received_callback(goal)
         return
-
-    def get_preemption_status(self):
-        preempted = False
-        if self._action_server.is_preempt_requested():
-            rospy.loginfo(self.action_goal + " Action Preemepted")
-            self._action_server.set_preempted()
-            preempted = True
-        return preempted
-
-    def get_goal_received(self):
-        if self.goal_received:
-            received = True
-            rospy.loginfo("The goal has been received:" + str(received))
-        else:
-            received = False
-        return received
 
     def get_request_data(self):
         """Return Request Data"""
@@ -68,7 +55,7 @@ class HarmoniActionServer(object):
         return(request_data)
 
     def send_feedback(self, state):
-        """ Send the feedback"""
+        """ Send the state as feedback"""
         self._feedback.action_type = self.action_goal
         self._feedback.state = state
         self._action_server.publish_feedback(self._feedback)
@@ -81,5 +68,24 @@ class HarmoniActionServer(object):
         self._result.do_action = do_action
         self._result.message = message
         self._action_server.set_succeeded(self._result)
-        rospy.loginfo("The action %i  have been set to succeded" %self._result.action_type)
+        rospy.loginfo("The action %i  have been set to succeded" % self._result.action_type)
         return
+
+    # is this useful?
+    def get_preemption_status(self):
+        """ Check preemption status on action server and log if preempted"""
+        if self._action_server.is_preempt_requested():
+            rospy.loginfo(self.action_goal + " Action Preemepted")
+            self._action_server.set_preempted()
+            return True
+        else:
+            return False
+
+    # is this useful?
+    def get_goal_received(self):
+        """ Check if goal has been recieved and log if true"""
+        if self.goal_received:
+            rospy.loginfo(f"The goal for {self.action_goal} has been received")
+            return True
+        else:
+            return False
