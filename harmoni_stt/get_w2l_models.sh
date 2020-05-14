4faf8ff19561d978fbc1353c87d50a45454228bc
chrismbirmingham <cmbirmingham19@gmail.com>
2020-05-14 12:10:11 -0700

    Misc. cleanup

--- harmoni_stt/get_w2l_models.sh
+++ harmoni_stt/get_w2l_models.sh
@@ -2,7 +2,7 @@
 
 echo "This script should be run from the HARMONI directory in order to place the models in a parallel directory"
 
-cd ..
+cd ../..
 mkdir -p model/w2l
 cd model/w2l
 for f in acoustic_model.bin tds_streaming.arch decoder_options.json feature_extractor.bin language_model.bin lexicon.txt tokens.txt ; do wget http://dl.fbaipublicfiles.com/wav2letter/inference/examples/model/${f} ; done
