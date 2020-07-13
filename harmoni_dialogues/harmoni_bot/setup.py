3204efcb3fc324213e92198ba0027f5ae78816a9
micolspitale93 <micol.spitale@gmail.com>
2020-07-13 12:24:32 +0200

    rename harmoni_lex into harmoni_bot, added google service not working yet.

--- harmoni_dialogues/harmoni_bot/setup.py
+++ harmoni_dialogues/harmoni_bot/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['harmoni_lex'],
+    packages=['harmoni_bot'],
     package_dir={'': 'scripts'},
 )
 
