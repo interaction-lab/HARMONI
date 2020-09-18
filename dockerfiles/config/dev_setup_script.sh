bafd171fde574b918403ba99e1d9feb8909b622a
Chris <cmbirmingham19@gmail.com>
2020-09-17 23:48:40 -0700

    WIP Images build but need testing. Only one compose file updated so far

--- dockerfiles/config/dev_setup_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-alias gs="git status"
-alias gl="git log --pretty=oneline --graph"
-alias gb="git branch -a"
-alias gg="git gui"
-alias code="code --user-data-dir /root/.visual_code/"
