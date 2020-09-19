0dfdf216e2225eb66e0e3f36bca51dbfc0bc9332
Chris <cmbirmingham19@gmail.com>
2020-09-19 13:11:22 -0700

    Working noetic but kinetic still crashes (will likely need to build from source)

---
+++ run_compose.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+xhost +local:
+
+export ROS_DISTRO="kinetic"
+
+docker-compose up
+# docker-compose -f docker-compose-core.yml up
+# docker-compose -f docker-compose-hardware.yml up
+
