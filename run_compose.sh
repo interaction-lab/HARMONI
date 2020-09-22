1deec7d3313f54bce2f233bfe041aff54a1aaa34
Unknown <cmbirmingham19@gmail.com>
2020-09-21 20:39:33 -0700

    Working and tested kinetic and noetic

--- run_compose.sh
+++ run_compose.sh
@@ -4,7 +4,7 @@ xhost +local:
 
 export ROS_DISTRO="kinetic"
 
-docker-compose up
+docker-compose up --remove-orphans
 # docker-compose -f docker-compose-core.yml up
 # docker-compose -f docker-compose-hardware.yml up
 
