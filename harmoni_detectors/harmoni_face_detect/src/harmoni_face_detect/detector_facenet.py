fdb5ed33cfded8b170ba6cc1466db4e5c6f8b1dd
Wen Chen <36430683+WayneUSC@users.noreply.github.com>
2020-10-05 11:14:54 -0700

    Add files via upload

--- harmoni_detectors/harmoni_face_detect/src/harmoni_face_detect/detector_facenet.py
+++ harmoni_detectors/harmoni_face_detect/src/harmoni_face_detect/detector_facenet.py
@@ -35,7 +35,7 @@ class FacenetFaceDetector(HarmoniServiceManager):
     # UPSAMPLING = 0
     # DEFAULT_RATE = 10 # Hz
 
-    def __init__(self, name, param, detector_threshold=0, mtcnn):
+    def __init__(self, name, param, detector_threshold=0):
         super().__init__(name)
         self._upsampling = param["up_sampling"]
         self._rate = param["rate_frame"]
@@ -58,7 +58,6 @@ class FacenetFaceDetector(HarmoniServiceManager):
         # self._hogFaceDetector = dlib.get_frontal_face_detector()
         # self._cv_bridge = CvBridge()
         self.state = State.INIT
-        self.mtcnn = mtcnn
 
     def start(self, rate):
         """
@@ -84,28 +83,73 @@ class FacenetFaceDetector(HarmoniServiceManager):
     def pause(self):
         self.stop()
 
-    def paint(self, frame, boxes, probability, landmarks):
-        try:
-            for box, probability, ld in zip(boxes, probability, landmarks):
-                cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (0, 0, 255), 10)
-        except:
-            pass
-        return frame
+    # def paint(self, frame, boxes, probability, landmarks):
+    #     try:
+    #         for box, probability, ld in zip(boxes, probability, landmarks):
+    #             cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (0, 0, 255), 10)
+    #     except:
+    #         pass
+    #     return frame
+    #
+    # def run(self):
+    #     cap = cv2.VideoCapture(0)
+    #     while True:
+    #         ret, img = cap.read()
+    #         try:
+    #             boxes, probability, landmarks = self.mtcnn.detect(img, landmarks=True)
+    #             self.paint(img, boxes, probability, landmarks)
+    #         except:
+    #             pass
+    #         cv2.imshow('img', img)
+    #         if cv2.waitKey(1) & 0xFF == ord('q'):
+    #             break
+    #     cap.release()
+    #     cv2.destroyAllWindows()
 
-    def run(self):
-        cap = cv2.VideoCapture(0)
-        while True:
-            ret, img = cap.read()
-            try:
-                boxes, probability, landmarks = self.mtcnn.detect(img, landmarks=True)
-                self.paint(img, boxes, probability, landmarks)
-            except:
-                pass
-            cv2.imshow('img', img)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        cap.release()
-        cv2.destroyAllWindows()
+    def detect_callback(self, image):
+        """Uses image to detect and publish face info.
+        Args:
+            image(Image): the image we want to run face detection on.
+        """
+        frame = self._cv_bridge.imgmsg_to_cv2(image, desired_encoding="passthrough")
+        if frame is not None:
+            h, w, _ = frame.shape
+            boxes, probs, landmarks = MTCNN().detect(frame, landmarks=True)
+            # help(MTCNN)
+            faces = []
+            for i, (box, probs) in enumerate(zip(boxes, probs)):
+                x1 = box[0]
+                y1 = box[1]
+                x2 = box[2]
+                y2 = box[3]
+                cx = (box[0] + box[2]) / 2
+                cy = (box[1] + box[3]) / 2
+
+                faces.append(
+                    Object2D(
+                        width=w,
+                        height=h,
+                        id=i,
+                        center_x=cx,
+                        center_y=cy,
+                        topleft_x=x1,
+                        topleft_y=y1,
+                        botright_x=x2,
+                        botright_y=y2,
+                        confidence=probs,
+                    )
+                )
+
+                # cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (0, 0, 255), 10)
+                # cv2.circle(frame, tuple((int(cx), int(cy))), 5, (0, 0, 255), -1)
+                # cv2.putText(frame, str(
+                #     probs), (box[2], box[3]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+
+            self._face_pub.publish(Object2DArray(faces))
+
+            # cv2.imshow('img', frame)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
 
 
 def main():
