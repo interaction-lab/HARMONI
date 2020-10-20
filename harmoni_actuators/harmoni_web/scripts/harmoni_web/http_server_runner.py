27bce4b825882a0d4de1ce3175e8a1661df78119
Michael Swan <rmswan@usc.edu>
2020-10-20 12:41:51 -0700

    keeping the same style for face and web. NOTE: copy pasted code needs to be fixed/generalized.

--- harmoni_actuators/harmoni_web/scripts/harmoni_web/http_server_runner.py
+++ harmoni_actuators/harmoni_web/scripts/harmoni_web/http_server_runner.py
@@ -38,10 +38,11 @@ if __name__ == "__main__":
     else:
         raise ValueError("Invalid directory input")
 
-    command = ["http-server"]
+    #FIXME this script is identical to the one in harmoni_face.
+    command = ["python3","-m","http.server"]
     if is_int(args[PORT_IDX]):
         port = args[PORT_IDX]
-        command += ["-p", port]
+        command += [port]
 
     p = subprocess.Popen(command)
     atexit.register(p.terminate)
