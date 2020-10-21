dcc9cf429c75bb47848d587df960eccd1b05a220
Michael Swan <rmswan@usc.edu>
2020-10-21 10:24:50 -0700

    run demo fix.

--- run_demo.sh
+++ run_demo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
   roscore \
-& rlharmoniservices \
-& rlhardwareservices \
-& rldemo
+& roslaunch harmoni_decision launcher.launch service:='harmoni' \
+& roslaunch harmoni_decision launcher.launch service:='hardware' \
+& roslaunch harmoni_pattern sequence_pattern.launch pattern_name:='demo' test:=true use_pattern_dialogue:=true use_pattern_multiple_choice:=false
