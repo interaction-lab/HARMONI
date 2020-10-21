a0bc51a33518b6d9e65cfced8670e079ec23c8db
Chris <cmbirmingham19@gmail.com>
2020-10-21 10:45:01 -0700

    Update Demo

--- run_demo.sh
+++ run_demo.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-  roscore \
-& roslaunch harmoni_decision launcher.launch service:='harmoni' \
-& roslaunch harmoni_decision launcher.launch service:='hardware' \
-& roslaunch harmoni_pattern sequence_pattern.launch pattern_name:='demo' test:=true use_pattern_dialogue:=true use_pattern_multiple_choice:=false
+
+roscore \
+ & sleep 5 \
+&& roslaunch harmoni_decision launcher.launch service:='harmoni' \
+  & sleep 3 \
+&& roslaunch harmoni_decision launcher.launch service:='hardware' \
+  & sleep 3 \
+&& roslaunch harmoni_pattern sequence_pattern.launch pattern_name:='demo' test:=true use_pattern_dialogue:=true use_pattern_multiple_choice:=false
