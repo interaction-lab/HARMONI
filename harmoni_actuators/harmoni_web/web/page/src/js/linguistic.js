352690e011dabb2755a29ce5e0ede17c3f84d56b
micolspitale93 <micol.spitale@gmail.com>
2021-02-15 13:02:14 +0100

    update transcription

--- harmoni_actuators/harmoni_web/web/page/src/js/linguistic.js
+++ harmoni_actuators/harmoni_web/web/page/src/js/linguistic.js
@@ -4,6 +4,7 @@ var recognizing = false;
 var final_transcript = '';
 var ignore_onend;
 var start_timestamp;
+var state="IDLE"
 
 
 function record(){
@@ -18,7 +19,7 @@ function record(){
       upgrade();
     } else {
       recognition = new webkitSpeechRecognition();
-      recognition.continuous = true;
+      recognition.continuous = false;
       recognition.interimResults = true;
       recognition.onstart = function() {
         recognizing = true;
@@ -47,14 +48,20 @@ function record(){
       };
       recognition.onend = function() {
         recognizing = false;
-        if (ignore_onend) {
-          return;
+        if(state=="SPEAK"){
+          recognition.start();
+          if (ignore_onend) {
+            return;
+          }  
+        }else{
+          start_img.src ='data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2024%2024%22%3E%3Cpath%20fill%3D%22none%22%20d%3D%22M0%200h24v24H0V0z%22%2F%3E%3Cpath%20d%3D%22M12%2015c1.66%200%202.99-1.34%202.99-3L15%206c0-1.66-1.34-3-3-3S9%204.34%209%206v6c0%201.66%201.34%203%203%203zm-1.2-9.1c0-.66.54-1.2%201.2-1.2s1.2.54%201.2%201.2l-.01%206.2c0%20.66-.53%201.2-1.19%201.2s-1.2-.54-1.2-1.2V5.9zm6.5%206.1c0%203-2.54%205.1-5.3%205.1S6.7%2015%206.7%2012H5c0%203.41%202.72%206.23%206%206.72V22h2v-3.28c3.28-.48%206-3.3%206-6.72h-1.7z%22%2F%3E%3C%2Fsvg%3E';
         }
-        start_img.src ='data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2024%2024%22%3E%3Cpath%20fill%3D%22none%22%20d%3D%22M0%200h24v24H0V0z%22%2F%3E%3Cpath%20d%3D%22M12%2015c1.66%200%202.99-1.34%202.99-3L15%206c0-1.66-1.34-3-3-3S9%204.34%209%206v6c0%201.66%201.34%203%203%203zm-1.2-9.1c0-.66.54-1.2%201.2-1.2s1.2.54%201.2%201.2l-.01%206.2c0%20.66-.53%201.2-1.19%201.2s-1.2-.54-1.2-1.2V5.9zm6.5%206.1c0%203-2.54%205.1-5.3%205.1S6.7%2015%206.7%2012H5c0%203.41%202.72%206.23%206%206.72V22h2v-3.28c3.28-.48%206-3.3%206-6.72h-1.7z%22%2F%3E%3C%2Fsvg%3E';
+        
+        /*start_img.src ='data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2024%2024%22%3E%3Cpath%20fill%3D%22none%22%20d%3D%22M0%200h24v24H0V0z%22%2F%3E%3Cpath%20d%3D%22M12%2015c1.66%200%202.99-1.34%202.99-3L15%206c0-1.66-1.34-3-3-3S9%204.34%209%206v6c0%201.66%201.34%203%203%203zm-1.2-9.1c0-.66.54-1.2%201.2-1.2s1.2.54%201.2%201.2l-.01%206.2c0%20.66-.53%201.2-1.19%201.2s-1.2-.54-1.2-1.2V5.9zm6.5%206.1c0%203-2.54%205.1-5.3%205.1S6.7%2015%206.7%2012H5c0%203.41%202.72%206.23%206%206.72V22h2v-3.28c3.28-.48%206-3.3%206-6.72h-1.7z%22%2F%3E%3C%2Fsvg%3E';
         if (!final_transcript) {
           showInfo('info_start');
           return;
-        }
+        }*/
         showInfo('');
         /*if (window.getSelection) {
           window.getSelection().removeAllRanges();
@@ -67,7 +74,7 @@ function record(){
         var interim_transcript = '';
         for (var i = event.resultIndex; i < event.results.length; ++i) {
           if (event.results[i].isFinal) {
-            final_transcript += event.results[i][0].transcript;
+            final_transcript = event.results[i][0].transcript;
           } else {
             interim_transcript += event.results[i][0].transcript;
           }
@@ -85,14 +92,16 @@ function upgrade() {
 function startButton(event) {
     console.log("Start Button clicked")
   if (recognizing) {
+    state="STOP"
     recognition.stop();
     showInfo(final_transcript)
     if (final_transcript!=""){
-      var body={transcript:final_transcript}
+      var body={transcript:final_transcript.toString()}
       user_response_publisher.publish({data: JSON.stringify(body)})
     }
     return;
   }
+  state="SPEAK"
   final_transcript = '';
   recognition.lang = lang;
   recognition.start();
