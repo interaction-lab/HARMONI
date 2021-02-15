178cb2d8ff5996c2e93e537162e0427f5d32159c
micolspitale93 <micol.spitale@gmail.com>
2021-02-15 10:46:11 +0100

    fix bugs.

--- harmoni_actuators/harmoni_web/scripts/harmoni_web/server_to_start_face.py
+++ harmoni_actuators/harmoni_web/scripts/harmoni_web/server_to_start_face.py
@@ -6,7 +6,7 @@ import os
 
 
 def callback_srv(data):
-    os.system("pkill luakit")
+    os.system("pkill init")
     shell_command = "export DISPLAY=:0 && luakit http://192.168.100.2:8081"
     process = subprocess.Popen(shell_command, shell=True)
     # process = subprocess.Popen("echo hi", shell=True)
