5f1411a396df1ca8ab1f99f79ead94ad1a22c45e
pavmassimo <pavmassimo@gmail.com>
2021-04-22 12:10:51 +0200

    corrected pupils dimension, now it should be good

--- harmoni_actuators/harmoni_face/web/src/js/run.js
+++ harmoni_actuators/harmoni_face/web/src/js/run.js
@@ -3,7 +3,7 @@ startFace(
     '', //ros_master_uri
     .009, //cm per pixel
     0, //viseme compression factor
-    '#ffffff', '#18970B', 75, 40, 200, 80, .7, "round", //eyes (white_color, iris_color, size, height, separation, iris_size, pupil_scale, pupil_shape)
+    '#ffffff', '#18970B', 75, 40, 200, 40, .7, "round", //eyes (white_color, iris_color, size, height, separation, iris_size, pupil_scale, pupil_shape)
     75, 65, .4, //eyelids (width, height, arch)
     '#ff99cc', 0, 10, 0, 0, //nose (color, x, y, width, height)
     '#2c241b', 8, -30, 175, 10, 9, 1, 4, 50.1, 500.1, //mouth (color, x, y, width, height, thickness, opening, dimple_size, ulip_h_scale, llip_h_scale)
