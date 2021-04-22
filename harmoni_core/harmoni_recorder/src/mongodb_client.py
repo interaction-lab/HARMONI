f32091785548b0d26f5fc974d0fc12975cf6849e
micolspitale93 <micol.spitale@gmail.com>
2021-04-22 15:13:06 +0200

    added mongodb client for harmoni + docker compose with mongo and mongo-express containers.

---
+++ harmoni_core/harmoni_recorder/src/mongodb_client.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Common Imports
+import rospy
+import roslib
+import pymongo  # package for working with MongoDB
+
+
+class MongoDBClient:
+
+    def __init__(self, client_uri, username, password):
+        self.client = self.mongodb_connect(client_uri, username, password)
+
+    def mongodb_connect(self, client_uri,username, password):
+        try:
+            client = pymongo.MongoClient(client_uri,
+                        username=username,
+                        password=password)
+            return client
+        except pymongo.errors.ConnectionFailure:
+            print("Failed to connect to server {}".format(client_uri))
+
+    def get_client(self):
+        return self.client
+
