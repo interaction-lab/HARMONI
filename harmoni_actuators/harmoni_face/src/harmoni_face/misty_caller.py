c194353a85186ced57c049c96f4fcbc35a9a7cd7
pavmassimo <pavmassimo@gmail.com>
2021-05-03 11:21:05 +0200

    harmoni_face working, test too

--- harmoni_actuators/harmoni_face/src/harmoni_face/misty_caller.py
+++ harmoni_actuators/harmoni_face/src/harmoni_face/misty_caller.py
@@ -5,6 +5,7 @@ import requests
 from requests.exceptions import Timeout
 import socket
 import sys
+from subprocess import check_output
 
 
 def is_int(value):
@@ -20,15 +21,16 @@ PORT_IDX = 2
 if __name__ == "__main__":
     rospy.init_node("misty_caller")
     robot_ip = rospy.get_param("/robot_ip")
-    
-    hostname = socket.gethostname()
-    local_ip = socket.gethostbyname(hostname)
+
+    local_ip = str(check_output(['hostname', '--all-ip-addresses']).split()[0])[2:-1]
 
     args = sys.argv
     if is_int(args[PORT_IDX]):
         port = args[PORT_IDX]
 
-    payload = {'URL': 'http://' + local_ip + ':' + port}
+    payload = {'URL': 'http://' + local_ip + ':' + port ,
+                'layer': 'DefaultWebViewLayer'}
+    print(payload['URL'])
 
     try:
         response = requests.post('http://{}/api/webviews/display'.format(robot_ip), 
