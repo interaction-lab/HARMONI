a8fddf271a4a459e0a29397c51d406dc47209722
Emily Zhou <zhouxemily@gmail.com>
2021-06-10 13:24:08 -0700

    Create DeepSpeech client
    Signed-off-by: Emily Zhou <zhouxemily@gmail.com>

--- harmoni_detectors/harmoni_stt/src/harmoni_stt/deepspeech_client.py
+++ harmoni_detectors/harmoni_stt/src/harmoni_stt/deepspeech_client.py
@@ -1,79 +1,106 @@
 #!/usr/bin/env python3
 import deepspeech
+import logging
 import numpy as np
 import os
 import pyaudio
+import sys
 import time
+
+logging.basicConfig(level=logging.INFO)
 
 
 class DeepSpeechClient:
 
-    def __init__(
-            self, 
-            model,
-            scorer_path,
-            alpha,
-            beta,
-            beam_width
-    ):
-        self._model = model
-        if not os.path.exists(scorer_path):
-            raise FileNotFoundError(f"Invalid scorer path: {scorer_path}")
-        self._model.enableExternalScorer(scorer_path)
-        self.model.setScorerAlphaBeta(alpha, beta)
-        self.model.setBeamWidth(beam_width)
+	def __init__(
+		self, 
+		model_file_path,
+		scorer_path,
+		alpha,
+		beta,
+		beam_width,
+		t_wait=0.5
+	):
+		if not os.path.exists(model_file_path):
+			raise FileNotFoundError(f"Invalid model path: {model_file_path}")
+		self._model = deepspeech.Model(model_file_path)
+
+		if not os.path.exists(scorer_path):
+			raise FileNotFoundError(f"Invalid scorer path: {scorer_path}")
+		self._model.enableExternalScorer(scorer_path)
+
+		self._model.setScorerAlphaBeta(alpha, beta)
+		self._model.setBeamWidth(beam_width)
         
         # Create a Streaming session
-        self.ds_stream = self.model.createStream()
+		self._ds_stream = self._model.createStream()
 
-        # Encapsulate DeepSpeech audio feeding into a callback for PyAudio
-        self.text_so_far = ''
-        self.t_start = time.time()
-        self.t_wait = .5
-        self.final_text = None
+		self._text = ''
+		self._t_start = time.time()
+		self._t_wait = t_wait
+		self._is_final = False
 
-    def listen(self):
-        # Feed audio to DeepSpeech in a callback to PyAudio
-        self.audio = pyaudio.PyAudio()
-        self.stream = self.audio.open(
-            format=pyaudio.paInt16,
-            channels=1,
-            rate=16000,
-            input=True,
-            frames_per_buffer=1024,
-            stream_callback=self.process_audio
-        )
+	def process_audio(self, data16):
+		self._is_final = False
+		self._ds_stream.feedAudioContent(data16)
+		text = self._ds_stream.intermediateDecode()
 
-        print('Please start speaking, when done press Ctrl-C ...')
-        self.stream.start_stream()
-        print("listening now")
+		try:
+			if text != self._text:
+				if text not in ["i ", "he ", "the "]:
+					logging.info('Interim text = {};'.format(text))
+				self._text = text
+				self._t_start = time.time()
 
-        return
+			elif text != '' and (time.time() - self._t_start > self._t_wait):
+				if text not in ["i ", "he ", "the "]:
+					logging.info("Finishing stream")
+					text = self._ds_stream.finishStream()
+					logging.info('Final text = {}.\n'.format(text))
+					self._text = text
+					self._is_final = True
 
-    def process_audio(self, in_data, frame_count, time_info, status):
-        data16 = np.frombuffer(in_data, dtype=np.int16)
-        self.ds_stream.feedAudioContent(data16)
-        text = self.ds_stream.intermediateDecode()
-        try:
-            if text != self.text_so_far:
-                if text not in ["i ", "he ", "the "]:
-                    print('Interim text = {};'.format(text))
-                self.text_so_far = text
-                self.t_start = time.time()
-            elif text != '' and (time.time() - self.t_start > self.t_wait):
-                if text not in ["i ", "he ", "the "]:
-                    print("Finishing stream")
-                    text = self.ds_stream.finishStream()
-                    print('Final text = {}.\n'.format(text))
-                    self.final_text = text
-                    self.ds_stream = self.model.createStream()
+		except Exception as e:
+			logging.info(f"Text: '{text}'; So far: '{self.text_so_far}")
+			logging.info(self._t_start)
+			raise e
 
-        except Exception as e:
-            print(f"Text: '{text}'; So far: '{self.text_so_far}")
-            print(self.t_start)
-            raise e
+		return text
 
-        return (in_data, pyaudio.paContinue)
+	def finish_stream(self):
+		text = self._ds_client.finishStream()
+		return text
 
-    def transcribe_from_file(self, audio_file):
-        pass
+	def transcribe_from_file(self, audio_file):
+		pass
+	
+	@property
+	def is_final(self):
+		return self._is_final
+
+
+if __name__ == "__main__":
+	import argparse
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("model_file_path")
+	parser.add_argument("scorer_path")
+
+	args = parser.parse_args()
+
+	model_file_path = args.model_file_path
+	scorer_path = args.scorer_path
+
+	lm_alpha = 0.75
+	lm_beta = 1.85
+	beam_width = 700
+    
+	ds_client = DeepSpeechClient(
+		model_file_path,
+		scorer_path,
+		lm_alpha,
+		lm_beta,
+		beam_width,
+		t_wait=2
+	)
+
