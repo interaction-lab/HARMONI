f82e66fc960b740667b987506b56d5eec14e6386
Emily Zhou <zhouxemily@gmail.com>
2021-06-28 14:41:08 -0700

    Add bot src files to path
    Signed-off-by: Emily Zhou <zhouxemily@gmail.com>

--- harmoni_dialogues/harmoni_bot/setup.py
+++ harmoni_dialogues/harmoni_bot/setup.py
@@ -1,4 +1,5 @@
 # ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+import os
 
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
@@ -6,7 +7,10 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['harmoni_bot'],
-    package_dir={'': 'src'},
+    package_dir={
+        '': 'src',
+        'harmoni_bot': os.path.join('src', 'harmoni_bot')
+    },
 )
 
 setup(**setup_args)
