48d9d972d1d1caef8a99e642a7f10622526d7454
Emily Zhou <zhouxemily@gmail.com>
2021-06-29 09:15:24 -0700

    Use relative paths for TTS setup
    Signed-off-by: Emily Zhou <zhouxemily@gmail.com>

--- harmoni_actuators/harmoni_tts/setup_tts.sh
+++ harmoni_actuators/harmoni_tts/setup_tts.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-TTS_DIR="/root/harmoni_catkin_ws/src/HARMONI/harmoni_actuators/harmoni_tts"
+echo "This script should be run from the HARMONI directory in order to place the models in a parallel directory"
 
-mkdir -p $TTS_DIR/content && cd $TTS_DIR/content \
+cd ../..
+mkdir -p model/tts && cd model/tts \
   && gdown --id 1dntzjWFg7ufWaTaFy80nRz-Tu02xWZos -O tts_model.pth.tar \
   && gdown --id 18CQ6G6tBEOfvCHlPqP8EBI4xWbrr9dBc -O config.json \
   && gdown --id 1Ty5DZdOc0F7OTGj9oJThYbL5iVu_2G0K -O vocoder_model.pth.tar \
   && gdown --id 1Rd0R_nRCrbjEdpOwq6XwZAktvugiBvmu -O config_vocoder.json
 
-cd .. && gdown --id 11oY3Tv0kQtxK_JPgxrfesa99maVXHNxU -O scale_stats.npy
-
+cd ../../src/HARMONI/harmoni_actuators/harmoni_tts
+gdown --id 11oY3Tv0kQtxK_JPgxrfesa99maVXHNxU -O scale_stats.npy
 sudo apt-get install espeak
 git clone https://github.com/coqui-ai/TTS
-
 cd TTS
 git checkout b1935c97
 pip install -r requirements.txt
