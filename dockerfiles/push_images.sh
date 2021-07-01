e817bfaf07e6228a91142bda60e2cc8b49e57550
chrismbirmingham <cmbirmingham19@gmail.com>
2021-07-01 15:04:54 -0700

    No longer building w2l (dep)

--- dockerfiles/push_images.sh
+++ dockerfiles/push_images.sh
@@ -4,7 +4,7 @@ docker push harmoniteam/harmoni:kinetic-base
 
 docker push harmoniteam/harmoni:kinetic-full
 
-docker push harmoniteam/harmoni:kinetic-w2l
+# docker push harmoniteam/harmoni:kinetic-w2l
 
 docker push harmoniteam/harmoni:kinetic-face_detect
 
