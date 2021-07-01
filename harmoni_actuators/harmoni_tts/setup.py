09944217f114c5cc095d28d6fab20524905e8b89
micolspitale93 <micol.spitale@gmail.com>
2021-07-01 17:50:23 +0200

    moved aws tts into nodes folder and removed src.

--- harmoni_actuators/harmoni_tts/setup.py
+++ harmoni_actuators/harmoni_tts/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['harmoni_tts'],
-    package_dir={'': 'src'},
+    package_dir={'': 'nodes'},
 )
 
 setup(**setup_args)
