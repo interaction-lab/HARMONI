a0159249141a53fd769efe4d190aaf5b4fa7b73b
Emily Zhou <zhouxemily@gmail.com>
2021-07-02 21:34:59 -0700

    Add test
    Signed-off-by: Emily Zhou <zhouxemily@gmail.com>

--- harmoni_dialogues/harmoni_bot/test/test_rasa_client.py
+++ harmoni_dialogues/harmoni_bot/test/test_rasa_client.py
@@ -11,11 +11,15 @@ class TestRasaClient(unittest.TestCase):
     def setUp(self):
         self.client = RasaClient("localhost", 5005)
 
-    @mock.patch("requests.post")
+    @mock.patch("harmoni_bot.rasa_client.requests.post")
     def test_get_rasa_response(self, mock_post):
         recipient_id = "test_user"
         text = "Welcome to the Interaction Lab!"
-
+        mock_post.return_value.json.return_value = [
+            {"recipient_id": recipient_id, "text": text}
+        ]
+        rasa_response = self.client.get_rasa_response("Hello world!")
+        assert rasa_response == text
 
 
 if __name__ == "__main__":
