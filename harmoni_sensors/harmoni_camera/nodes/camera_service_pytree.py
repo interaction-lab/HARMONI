11ace2e92585454ec0f269472cce6855c54831cb
Tharushi <taru97@hotmail.it>
2021-07-05 09:47:15 +0200

    first sketch of camera_service_pytree.py

--- harmoni_sensors/harmoni_camera/nodes/camera_service_pytree.py
+++ harmoni_sensors/harmoni_camera/nodes/camera_service_pytree.py
@@ -8,19 +8,15 @@ from harmoni_common_lib.constants import State
 from actionlib_msgs.msg import GoalStatus
 from harmoni_common_lib.action_client import HarmoniActionClient
 import harmoni_common_lib.helper_functions as hf
-from microphone_service import MicrophoneService
+from camera_service import CameraService
 # Other Imports
 from harmoni_common_lib.constants import SensorNameSpace
-from audio_common_msgs.msg import AudioData
-import pyaudio
-import wave
-import numpy as np
+
 # Specific Imports
 from harmoni_common_lib.constants import ActuatorNameSpace, ActionType, DialogueNameSpace
 from botocore.exceptions import BotoCoreError, ClientError
 from contextlib import closing
 from collections import deque 
-import soundfile as sf
 import numpy as np
 import boto3
 import re
@@ -34,7 +30,7 @@ import time
 
 import py_trees.console
 
-class MicrophoneServicePytree(py_trees.behaviour.Behaviour):
+class CameraServicePytree(py_trees.behaviour.Behaviour):
 
     #TODO tutte le print devono diventare console py_tree
     """
@@ -42,25 +38,25 @@ class MicrophoneServicePytree(py_trees.behaviour.Behaviour):
     true: opzione 1 (utilizzo come una classe python)
     false: opzione 2 (utilizzo mediate action_goal)
     """
+  
 
-
-    def __init__(self, name = "MicrophoneServicePytree"):
+    def __init__(self, name = "CameraServicePytree"):
         """
         Qui abbiamo pensato di chiamare soltanto 
         il costruttore del behaviour tree 
         """
         self.name = name
         self.mode = False
-        self.microphone_service = None
+        self.camera_service = None
         self.result_data = None
-        self.service_client_microphone = None
+        self.service_client_camera = None
         self.client_result = None
 
         self.blackboards = []
-        self.blackboard_microphone = self.attach_blackboard_client(name=self.name, namespace="harmoni_microphone")
-        self.blackboard_microphone.register_key("result_message", access=py_trees.common.Access.WRITE)
+        self.blackboard_camera = self.attach_blackboard_client(name=self.name, namespace="harmoni_camera")
+        self.blackboard_camera.register_key("result_message", access=py_trees.common.Access.WRITE)
 
-        super(MicrophoneServicePytree, self).__init__(name)
+        super(CameraServicePytree, self).__init__(name)
         self.logger.debug("%s.__init__()" % (self.__class__.__name__))
 
     def setup(self,**additional_parameters):
@@ -72,25 +68,24 @@ class MicrophoneServicePytree(py_trees.behaviour.Behaviour):
         """
         for parameter in additional_parameters:
             print(parameter, additional_parameters[parameter])  
-            if(parameter =="MicrophoneServicePytree_mode"):
+            if(parameter =="CameraServicePytree_mode"):
                 self.mode = additional_parameters[parameter]        
 
-        service_name = SensorNameSpace.microphone.name  # "microphone"
+        service_name = SensorNameSpace.camera.name  # "camera"
         instance_id = rospy.get_param("instance_id")  # "default"
         service_id = f"{service_name}_{instance_id}"
 
         params = rospy.get_param(service_name + "/" + instance_id + "_param/")
 
-        self.microphone_service = MicrophoneService(service_id, params) 
+        self.camera_service = CameraService(service_id, params) 
 
-        #TODO questo dobbiamo farlo nell'if 
         #rospy init node mi fa diventare un nodo ros
-        rospy.init_node("microphone_default", log_level=rospy.INFO)
+        #rospy.init_node("camera_default", log_level=rospy.INFO)
 
         if(not self.mode):
-            self.service_client_microphone = HarmoniActionClient(self.name)
+            self.service_client_camera = HarmoniActionClient(self.name)
             self.client_result = deque()
-            self.service_client_microphone.setup_client("microphone_default", 
+            self.service_client_camera.setup_client("camera_default", 
                                                 self._result_callback,
                                                 self._feedback_callback)
             self.logger.debug("Behavior interface action clients have been set up!")
@@ -110,33 +105,33 @@ class MicrophoneServicePytree(py_trees.behaviour.Behaviour):
         if(self.mode):
             pass
         else:
-            if self.service_client_microphone.get_state() == GoalStatus.LOST:
-                self.logger.debug(f"Sending goal to {self.microphone_service}")
+            if self.service_client_camera.get_state() == GoalStatus.LOST:
+                self.logger.debug(f"Sending goal to {self.camera_service}")
                 # Send request for each sensor service to set themselves up
-                self.service_client_microphone.send_goal(
+                self.service_client_camera.send_goal(
                     action_goal=ActionType["ON"].value,
                     optional_data="Setup",
                     wait="",
                 )
-                self.logger.debug(f"Goal sent to {self.microphone_service}")
-                self.blackboard_microphone.result_message = "RUNNING"
+                self.logger.debug(f"Goal sent to {self.camera_service}")
+                self.blackboard_camera.result_message = "RUNNING"
                 new_status = py_trees.common.Status.RUNNING
             else:
-                if self.service_client_microphone.get_state() != GoalStatus.LOST:
+                if self.service_client_camera.get_state() != GoalStatus.LOST:
                     #TODO qui succede che appena ci arriva un audio nuovo noi sovrascriviamo, cosa non del tutto giusta
                     if len(self.client_result) > 0:
                         self.result_data = self.client_result.popleft()["data"]
-                        self.blackboard_microphone.result_message = "RUNNING"
+                        self.blackboard_camera.result_message = "RUNNING"
                         new_status = py_trees.common.Status.RUNNING
                     else:
                         #se siamo qui vuol dire che il risultato ancora non c'è, dunque
                         #si è rotto tutto o dobbiamo solo aspettare?
                         #incerti di questa riga, vedi 408 sequential_pattern.py
-                        if(self.microphone_service.state == State.FAILED):
-                            self.blackboard_microphone.result_message = "FAILURE"
+                        if(self.camera_service.state == State.FAILED):
+                            self.blackboard_camera.result_message = "FAILURE"
                             new_status = py_trees.common.Status.FAILURE
                         else:
-                            self.blackboard_microphone.result_message = "RUNNING"
+                            self.blackboard_camera.result_message = "RUNNING"
                             new_status = py_trees.common.Status.RUNNING
                 else:
                     new_status = py_trees.common.Status.FAILURE
@@ -192,21 +187,21 @@ def main():
 
     py_trees.logging.level = py_trees.logging.Level.DEBUG
     
-    blackboardProva = py_trees.blackboard.Client(name="blackboardProva", namespace="harmoni_microphone")
+    blackboardProva = py_trees.blackboard.Client(name="blackboardProva", namespace="harmoni_camera")
     blackboardProva.register_key("result_message", access=py_trees.common.Access.READ)
 
 
     print(blackboardProva)
 
-    microphonePyTree = MicrophoneServicePytree("MicrophoneServicePytreeTest")
+    cameraPyTree = CameraServicePytree("CameraServicePytreeTest")
 
     additional_parameters = dict([
-        ("MicrophoneServicePytree_mode",False)])
+        ("CameraServicePytree_mode",False)])
 
-    microphonePyTree.setup(**additional_parameters)
+    cameraPyTree.setup(**additional_parameters)
     try:
         for unused_i in range(0, 3):
-            microphonePyTree.tick_once()
+            cameraPyTree.tick_once()
             time.sleep(0.5)
             print(blackboardProva)
         print("\n")
