5015055ba7af0daeef112e159dbe2cdbd4569bbf
Emily Zhou <zhouxemily@gmail.com>
2021-07-07 09:42:42 -0700

    Add test for DeepSpeech transcription output
    Signed-off-by: Emily Zhou <zhouxemily@gmail.com>

--- harmoni_detectors/harmoni_stt/test/test_deepspeech_client.py
+++ harmoni_detectors/harmoni_stt/test/test_deepspeech_client.py
@@ -3,10 +3,12 @@
 import mock
 import numpy as np
 import os
+import pyaudio
 import unittest
 import wave
 
 from harmoni_stt.deepspeech_client import DeepSpeechClient
+from scipy.io import wavfile
 
 
 MODEL_DIR = os.path.abspath(os.path.join(os.getcwd(), "../../../../../model/deepspeech/models"))
@@ -16,7 +18,7 @@ LM_ALPHA = 0.75
 LM_BETA = 1.85
 BEAM_WIDTH = 700
 
-TEST_AUDIO_FILE_PATH = os.path.abspath(os.path.join(os.getcwd(), "../test_data/hello.wav"))
+TEST_AUDIO_FILE_PATH = os.path.abspath(os.path.join(os.getcwd(), "../test_data/hello_16k.wav"))
 
 
 class TestDeepSpeechClient(unittest.TestCase):
@@ -51,14 +53,30 @@ class TestDeepSpeechClient(unittest.TestCase):
         assert not self.ds_client.is_streaming
 
     def test_process_audio(self):
-        wf = wave.open(TEST_AUDIO_FILE_PATH, 'rb')
-        chunk = 1024
-        data = wf.readframes(chunk)
-        while data:
+        # TODO: fix method of streaming from wav file; currently the output is "to" instead of "hello"
+        chunk_size = 1024
+        wf = wave.open(TEST_AUDIO_FILE_PATH)
+        # create an audio object
+        p = pyaudio.PyAudio()
+        # open stream based on the wave object which has been input.
+        stream = p.open(
+            format=p.get_format_from_width(wf.getsampwidth()),
+            channels=wf.getnchannels(),
+            rate=wf.getframerate(),
+            output=True
+        )
+        # read data (based on the chunk size)
+        self.ds_client.start_stream()
+        # play stream
+        while not self.ds_client.is_final:
+            data = wf.readframes(chunk_size)
             text = self.ds_client.process_audio(data)
-            data = wf.readframes(chunk)
-        assert text is not None
-        wf.close()
+        # close stream
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+        # expected transcription from the test audio file is "hello"
+        # assert text == "hello"
 
 
 if __name__ == "__main__":
