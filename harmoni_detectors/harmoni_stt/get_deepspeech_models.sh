54af6b452067ad63e2088ddd342a6c7626378bc2
Emily Zhou <zhouxemily@gmail.com>
2021-07-08 10:21:34 -0700

    Install STT models in harmoni_models package
    Signed-off-by: Emily Zhou <zhouxemily@gmail.com>

--- harmoni_detectors/harmoni_stt/get_deepspeech_models.sh
+++ harmoni_detectors/harmoni_stt/get_deepspeech_models.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-echo "This script should be run from the HARMONI directory in order to place the models in a parallel directory"
+echo "This script should be run from the HARMONI directory in order to place the models in the harmoni_models package"
 
-cd ../..
-mkdir -p model/deepspeech
-cd model/deepspeech
-wget -P models "https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.pbmm"
-wget -P models "https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.scorer"
+mkdir -p harmoni_models/stt && cd harmoni_models/stt
+wget "https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.pbmm"
+wget "https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.scorer"
