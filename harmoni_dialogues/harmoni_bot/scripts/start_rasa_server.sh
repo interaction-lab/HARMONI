11b32130776f187cb332dea7a0f4443aaa22ce03
Emily Zhou <zhouxemily@gmail.com>
2021-07-27 13:18:55 -0700

    Update script to start rasa server
    Signed-off-by: Emily Zhou <zhouxemily@gmail.com>

--- harmoni_dialogues/harmoni_bot/scripts/start_rasa_server.sh
+++ harmoni_dialogues/harmoni_bot/scripts/start_rasa_server.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-RASA_ASSISTANT=$(rosparam get /rasa/default_param/rasa_assistant)
-RASA_DIR=$(rosparam get /harmoni_bot_dir)
+shopt -s dotglob
+shopt -s nullglob
 
-if [ "$RASA_ASSISTANT" == "rasa_example" ] || [ "$RASA_ASSISTANT" == "rasa_greeter" ]
-  then
-    cd "$RASA_DIR"/src/"$RASA_ASSISTANT"
+RASA_ASSISTANT=$(rosparam get /rasa/default_param/rasa_assistant)
+RASA_MODEL_DIR=$(rosparam get /bot_model_dir)
+RASA_BOTS=("$RASA_MODEL_DIR/bot/"*)
+
+found=0
+for dir in "${RASA_BOTS[@]}" ; do
+    dir=$(basename "$dir")
+    [[ $dir = "$RASA_ASSISTANT" ]] && found=1
+done
+
+if [ $found == 1 ]; then
+    cd "$RASA_MODEL_DIR"/bot/"$RASA_ASSISTANT" || exit
     rasa train && rasa run
 else
-  echo "Not a valid Rasa bot"
+    echo "Not a valid Rasa bot"
 fi
