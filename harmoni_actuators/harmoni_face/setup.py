9833cc36e3a97402ffad5f9a9d904e399848a6bd
micolspitale93 <micol.spitale@gmail.com>
2021-09-06 16:03:01 +0200

    pytree.test working

--- harmoni_actuators/harmoni_face/setup.py
+++ harmoni_actuators/harmoni_face/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=["harmoni_face"], 
-    package_dir={"": "src", "":"nodes"},
+    package_dir={"": "src"},
 )
 
 setup(**setup_args)
