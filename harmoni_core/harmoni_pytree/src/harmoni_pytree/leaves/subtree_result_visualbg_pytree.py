32bfdd1ef8302c77d2de853b3b13e50c05d2661f
tharu17 <taru97@hotmail.it>
2021-09-12 13:44:27 +0200

    adding useful modules for our activity

---
+++ harmoni_core/harmoni_pytree/src/harmoni_pytree/leaves/subtree_result_visualbg_pytree.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import py_trees
+import random
+
+
+class SubTreeResultVisualBg(py_trees.behaviour.Behaviour):
+    def __init__(self, name):
+        """
+        Minimal one-time initialisation. A good rule of thumb is
+        to only include the initialisation relevant for being able
+        to insert this behaviour in a tree for offline rendering to
+        dot graphs.
+
+        Other one-time initialisation requirements should be met viass
+        the setup() method.
+        """
+        super(SubTreeResultVisualBg, self).__init__(name)
+
+    def setup(self):
+        """
+        When is this called?
+          This function should be either manually called by your program
+          to setup this behaviour alone, or more commonly, via
+          :meth:`~py_trees.behaviour.Behaviour.setup_with_descendants`
+          or :meth:`~py_trees.trees.BehaviourTree.setup`, both of which
+          will iterate over this behaviour, it's children (it's children's
+          children ...) calling :meth:`~py_trees.behaviour.Behaviour.setup`
+          on each in turn.
+
+          If you have vital initialisation necessary to the success
+          execution of your behaviour, put a guard in your
+          :meth:`~py_trees.behaviour.Behaviour.initialise` method
+          to protect against entry without having been setup.
+
+        What to do here?
+          Delayed one-time initialisation that would otherwise interfere
+          with offline rendering of this behaviour in a tree to dot graph
+          or validation of the behaviour's configuration.
+
+          Good examples include:
+
+          - Hardware or driver initialisation
+          - Middleware initialisation (e.g. ROS pubs/subs/services)
+          - A parallel checking for a valid policy configuration after
+            children have been added or removed
+        """
+        self.logger.debug("  %s [SubTreeResultVisualBg::setup()]" % self.name)
+
+    def initialise(self):
+        """
+        When is this called?
+          The first time your behaviour is ticked and anytime the
+          status is not RUNNING thereafter.
+
+        What to do here?
+          Any initialisation you need before putting your behaviour
+          to work.
+        """
+        self.logger.debug("  %s [SubTreeResultVisualBg::initialise()]" % self.name)
+
+    def update(self):
+        """
+        When is this called?
+          Every time your behaviour is ticked.
+
+        What to do here?
+          - Triggering, checking, monitoring. Anything...but do not block!
+          - Set a feedback message
+          - return a py_trees.common.Status.[RUNNING, SUCCESS, FAILURE]
+        """
+        self.logger.debug("  %s [SubTreeResultVisualBg::update()]" % self.name)
+        ready_to_make_a_decision = random.choice([True, False])
+        decision = random.choice([True, False])
+        if not ready_to_make_a_decision:
+            return py_trees.common.Status.RUNNING
+        elif decision:
+            self.feedback_message = "We are not bar!"
+            return py_trees.common.Status.SUCCESS
+        else:
+            self.feedback_message = "Uh oh"
+            return py_trees.common.Status.FAILURE
+
+    def terminate(self, new_status):
+        """
+        When is this called?
+           Whenever your behaviour switches to a non-running state.
+            - SUCCESS || FAILURE : your behaviour's work cycle has finished
+            - INVALID : a higher priority branch has interrupted, or shutting down
+        """
+        self.logger.debug("  %s [SubTreeResultVisualBg::terminate().terminate()][%s->%s]" % (self.name, self.status, new_status))
