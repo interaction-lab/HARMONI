f7bc59d40be78e89297c00a3ddb12d5a75c595b4
CorradoPacelli <corrado.pacel@gmail.com>
2021-10-08 15:34:57 +0200

    fixing camera

--- harmoni_core/harmoni_pytree/src/harmoni_pytree/subtrees/on_modules.py
+++ harmoni_core/harmoni_pytree/src/harmoni_pytree/subtrees/on_modules.py
@@ -5,10 +5,11 @@ from harmoni_pytree.leaves.camera_service import CameraServicePytree
 from harmoni_pytree.leaves.microphone_service import MicrophoneServicePytree
 
 def create_root():
-    camera = CameraServicePytree(name="Camera")
+    #camera = CameraServicePytree(name="Camera")
     microphone=MicrophoneServicePytree("MicrophoneMainActivity")
     #external_camera = CameraServicePytree(name="ExternalCamera")
     root = py_trees.composites.Parallel("Parallel_On_Modules")
-    root.add_children([camera, microphone])
+    #root.add_children([camera, microphone])
+    root.add_child([microphone])
 
     return root
